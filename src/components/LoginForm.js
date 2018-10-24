1286c26f7b1d851cf77479d9b8af59bb3c5401b4
derrick b <dbozich1@gmail.com>
2018-10-24 18:53:22 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,7 +18,7 @@ class LoginForm extends React.Component {
 
   handleSubmit = event =>{
     event.preventDefault();
-    if (this.props.username != '' && this.props.password != '') {
+    if (!this.props.username || !this.props.password) {
       return this.props.onSubmit({this.state.username, this.state.password})
     } else {
       return
